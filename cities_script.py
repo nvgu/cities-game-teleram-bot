b4d6e04fe5dbae955f9dff171e07e839012a138a
Nadya <yfdq@mail.ru>
2018-11-01 21:05:48 +0300

    fixed bugs

--- cities_script.py
+++ cities_script.py
@@ -2,4 +2,4 @@ import json
 with open('cities.json', encoding="utf8") as json_data:
     data = json.load(json_data)
     list_of_cities = data['city']
-    name = list_of_cities['name']
+    city = list_of_cities
--- gamebot.py
+++ gamebot.py
@@ -6,8 +6,8 @@ bot = telebot.TeleBot(config.token)
 def handle_text(message):
     user_message = input("Введите город")
     i = user_message[-1] 
-    for city in cities_script.name:
-        if i == city[0]:
-            bot.send_message(message.chat.id, city)
+    for city in cities_script.city:
+        if i == city['name'][0]:
+            bot.send_message(message.chat.id, city['name'])
 if __name__ == '__main__':
      bot.polling(none_stop=True)
