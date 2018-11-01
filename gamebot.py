8ae13ad60107dca4360010f98742e06dc9b01415
Nadya <yfdq@mail.ru>
2018-11-01 21:43:32 +0300

    fixed bugs

--- gamebot.py
+++ gamebot.py
@@ -4,11 +4,10 @@ import cities_script
 bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    user_message = message.text
+    user_message = str(message.text)
     i = user_message[-1] 
     for city in cities_script.city:
-        bot.send_message(message.chat.id, city['name'])
-        if i == city['name'][0]:
+        if i.lower() == city['name'][0].lower():
             bot.send_message(message.chat.id, city['name'])
 if __name__ == '__main__':
      bot.polling(none_stop=True)
